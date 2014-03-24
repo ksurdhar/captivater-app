b1fc98ee21df3d844166e5de28f69163f0440dc4
Kiran Surdhar <ksurdhar1@gmail.com>
2014-03-24 16:53:10 -0700

    fixed direction, added looping, updates highlight position.

--- captivater-app/app/assets/javascripts/views/textblocks_show.js
+++ captivater-app/app/assets/javascripts/views/textblocks_show.js
@@ -38,14 +38,25 @@ window.CaptivaterApp.Views.textblocksShow = Backbone.View.extend({
     var word_hash = this.model.currentImgs();
     var words = [];
     for(var k in word_hash) words.push(k);
+    words.reverse();
 
     var match = words[that._counter()]
     that.$el.find(".current_imgs").html(word_hash[match]);
+
+    if(this._counterNum > 0){
+      that.$el.find('.current_sentence').html(function(q, v) {
+        return v.replace('<span>'+ words[that._counter() - 1] +'</span>', words[that._counter() - 1]);
+      });
+    }
+
     that.$el.find('.current_sentence').html(function(q, v) {
       return v.replace(match, '<span>' + match + '</span>');
     });
 
     that._counterNum++;
+    if(that._counterNum === words.length + 1){
+      that._counterNum = 0
+    }
   },
 
   _counter: function(){
